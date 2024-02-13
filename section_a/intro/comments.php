5747f0dbfd3a1add4af7dd2abda50c5e19d86faa
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-02-14 04:19:37 +0700

    Finish basic programming instruction

---
+++ section_a/intro/comments.php
@@ -0,0 +1,19 @@
+<?php
+  /*
+    This page displays the memeber's name
+    and details of a current offer.
+  */
+?>
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Adding Comments to Your Code</title>
+  </head>
+  <body>
+    <h1>The Candy Store</h1>
+    <h2><?php echo 'Welcome Yutna'; // Show name ?></h2>
+    <?php echo '<p class="offer">Offer: 20% off</p>' ?>
+  </body>
+</html>
--- section_a/intro/echo.php
+++ section_a/intro/echo.php
@@ -3,9 +3,11 @@
   <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
+    <title>Echo Command</title>
   </head>
   <body>
-    <h1><?php echo "Hello, world!" ?></h1>
+    <h1>The Candy Store</h1>
+    <h2><?php echo 'Yutna\'s'; ?> page</h2>
+    <?php echo '<p class="offer">Offer: 20% off</p>' ?>
   </body>
 </html>
