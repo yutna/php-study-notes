e6ddb9f79fd3b4599a1cdb7bd4c1df5231e17215
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-02-18 21:12:52 +0700

    Update BASE_URL for my Mac setup

--- config/const.php
+++ config/const.php
@@ -1,4 +1,4 @@
 <?php
 
-define('BASE_URL', 'http://localhost/php-playground');
+define('BASE_URL', PHP_OS === 'Darwin' ? 'http://php-playground.test' : 'http://localhost/php-playground');
 define('BASE_PHP_AND_MYSQL_URL', BASE_URL . '/php-and-mysql');
