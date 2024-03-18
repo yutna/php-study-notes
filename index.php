817e9c9dcc1deffb482a6cda22dae568da016d27
Yutthana Siphuengchai <yutthana.s@outlook.com>
2024-03-18 12:25:34 +0700

    Add style for dark mode color

--- index.php
+++ index.php
@@ -7,6 +7,25 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>PHP Playground</title>
+    <style>
+        body {
+            color: white;
+            background-color: black;
+        }
+
+        a:link {
+            color: red;
+        }
+
+        a:visited {
+            color: pink;
+        }
+
+        a:link:active,
+        a:visited:active {
+            color: salmon;
+        }
+    </style>
 </head>
 
 <body>
